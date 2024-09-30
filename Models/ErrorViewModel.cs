d01060a88b95e0b30cda6f7f1471acce45d1fa32
Vanillien <ivanpaketov71@gmail.com>
2024-09-30 23:47:41 +0300

    f c

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Pastebin_2._0.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
